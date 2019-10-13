2639345bbf7fb51fcbfb7211ed4225134a14cc33
Chris Fuertes <cristhian.fuertes@correounivalle.edu.co>
2019-10-13 17:27:10 -0500

    Housekeeping

--- lib/teppelin/twitch_tv.ex
+++ lib/teppelin/twitch_tv.ex
@@ -1,6 +1,5 @@
 defmodule Teppelin.TwitchTV do
-
-  use GenServer 
+  use GenServer
 
   @base_url Application.get_env(:teppelin, :twitch_base_url)
   @client_id Application.get_env(:teppelin, :twitch_client_id)
@@ -13,79 +12,59 @@ defmodule Teppelin.TwitchTV do
   end
 
   def search_streams(search_term) do
-    IO.puts "Teppelin.TwitchTV.search_streams #{search_term}"
     GenServer.cast(@me, {:search_streams, search_term})
   end
 
-  
   def init([]) do
-    IO.puts "Teppelin.TwitchTV.init"
-    streams =  get_live_streams_aux()
-    TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams}) 
-    {:ok, 
-     %{ streams: streams,
-        search_term: nil}, 
-     @timeout }
+    streams = get_live_streams()
+    TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams})
+    {:ok, %{streams: streams, search_term: nil}, @timeout}
   end
 
   def handle_info(:timeout, %{search_term: search_term}) do
-    IO.puts "Teppelin.TwitchTV.handle_info 1 #{search_term}"
-    streams = get_live_streams_aux()
-    if search_term == nil do
-       TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams})
-    else
-       TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams |> filter_streams(search_term)})
-    end 
-   
-    {:noreply, %{ streams: streams,
-                  search_term: search_term}, 
-      @timeout}
-  end
+    streams = get_live_streams()
 
-  # def handle_info(:timeout, %{streams: streams, search_term: search_term} = state) do
-  #      IO.puts "Teppelin.TwitchTV.handle_info 2 #{search_term}"
-  #     streams = 
-  #       cond do
-  #         search_term == nil -> get_live_streams_aux()
-  #           true -> get_live_streams_aux() |> filter_streams(search_term)
-  #       end
-  #     TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams}) 
-  #    {:noreply, state, @timeout}
-  # end
+    if search_term == nil do
+      TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: streams})
+    else
+      TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{
+        streams: streams |> filter_streams(search_term)
+      })
+    end
+
+    {:noreply, %{streams: streams, search_term: search_term}, @timeout}
+  end
 
   def handle_cast({:search_streams, search_term}, %{streams: streams}) do
-    filtered_streams =  streams |> filter_streams(search_term)
-    IO.puts "Teppelin.TwitchTV.handle_cast search_term : #{search_term} size: #{length(filtered_streams)}"
-    TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{streams: filtered_streams}) 
-    {:noreply, %{ streams: streams, search_term: search_term }} 
+    filtered_streams = streams |> filter_streams(search_term)
+
+    TeppelinWeb.Endpoint.broadcast_from(self(), "twitch", "live_streams", %{
+      streams: filtered_streams
+    })
+
+    {:noreply, %{streams: streams, search_term: search_term}}
   end
 
-  def handle_call(_, _from, state) do
-    IO.puts "facepalm #{inspect state}"
-    {:reply, nil,  state}
-  end
-
-   defp get_live_streams_aux() do
+  defp get_live_streams() do
     full_url = "#{@base_url}/streams?stream_type=live"
-    headers = ["Client-ID": @client_id,
-               "Accept": @api_version,
-               "User-Agent": "Teppelin app"]
+    headers = ["Client-ID": @client_id, Accept: @api_version, "User-Agent": "Teppelin app"]
     {:ok, streams} = get(full_url, headers, :eager)
-    streams 
+    streams
   end
 
   defp filter_streams(streams, nil), do: streams
 
   defp filter_streams(streams, search_term) do
-     game = String.capitalize(search_term)
-     regex = ~r{#{game}}i
-     streams
-     |> Enum.filter( fn item ->
-        cond  do
-          Regex.run(regex, item[:game]) == nil -> false
-          true -> true 
-        end
-      end)
+    game = String.capitalize(search_term)
+    regex = ~r{#{game}}i
+
+    streams
+    |> Enum.filter(fn item ->
+      cond do
+        Regex.run(regex, item[:game]) == nil -> false
+        true -> true
+      end
+    end)
   end
 
   defp get(url, headers, :eager) do
@@ -93,18 +72,21 @@ defmodule Teppelin.TwitchTV do
     |> handle_response(:eager)
   end
 
-  defp handle_response({_,
-                        %{status_code: status_code, body: body}},
-                        :eager) do
-    streams = 
+  defp handle_response(
+         {_, %{status_code: status_code, body: body}},
+         :eager
+       ) do
+    streams =
       case check_for_errors(status_code, body) do
         {:error, _} -> []
         data -> data
       end
+
     {:ok, streams}
   end
 
   defp check_for_errors(200, body), do: body |> parse_data()
+
   defp check_for_errors(_, body), do: {:error, body}
 
   defp parse_data(data) do
@@ -112,7 +94,4 @@ defmodule Teppelin.TwitchTV do
     |> Poison.decode!(keys: :atoms)
     |> Map.get(:streams)
   end
-
- 
-
 end
--- lib/teppelin_web/live/twitch_streams_live.ex
+++ lib/teppelin_web/live/twitch_streams_live.ex
@@ -6,65 +6,51 @@ defmodule TeppelinWeb.TwitchStreamsLive do
   end
 
   def mount(_session, socket) do
-    IO.puts "mount"
     if connected?(socket) do
       TeppelinWeb.Endpoint.subscribe("twitch")
     end
-    {:ok, init_data(socket) }
+
+    {:ok, init_data(socket)}
   end
 
-  def handle_info(%Phoenix.Socket.Broadcast{event: "live_streams", topic: "twitch", payload: %{streams: streams}}, socket) do
-    IO.puts "TeppelinWeb.TwitchStreamsLive.handle_info 1 #{length(streams)}"
-    {:noreply, 
-     assign(socket, 
-           streams: streams, streams_count: length(streams))}
+  def handle_info(
+        %Phoenix.Socket.Broadcast{
+          event: "live_streams",
+          topic: "twitch",
+          payload: %{streams: streams}
+        },
+        socket
+      ) do
+    {:noreply, assign(socket, streams: streams, streams_count: length(streams))}
   end
-
-  # def handle_info({:search_streams, search_term}, socket) do
-  #    IO.puts "TeppelinWeb.TwitchStreamsLive.handle_info 2"
-  #   Teppelin.TwitchTV.search_streams(search_term)
-  #   {:noreply, assign(socket, search_term: search_term)}
-  # end
 
   def handle_info(:search, socket = %{assigns: %{search_term: search_term}}) do
-    IO.puts "TeppelinWeb.TwitchStreamsLive.handle_info #{search_term}"
     Teppelin.TwitchTV.search_streams(search_term)
     {:noreply, assign(socket, loading: false)}
   end
 
-
-  def handle_event("search", %{"value" => search_term}, socket = %{ assigns: %{ loading: false }}) do
-    IO.puts "loaiding = false"
+  def handle_event("search", %{"value" => search_term}, socket = %{assigns: %{loading: false}}) do
     timer_ref = Process.send_after(self(), :search, 1000)
-    {:noreply, 
-      assign(socket, 
-             search_term: search_term,
-             timer_ref: timer_ref,
-             loading: true) }
-  end
 
-  def handle_event("search",  %{"value" => search_term}, %{assigns: %{loading: true}} = socket) do
-    IO.puts "loaiding = true"
     {:noreply,
-     assign(socket, search_term: search_term )}
+     assign(socket,
+       search_term: search_term,
+       timer_ref: timer_ref,
+       loading: true
+     )}
   end
 
-  def handle_event("search", _params, socket) do
-    IO.puts "facepalm"
-    System.halt(0)
-    {:noreply, socket}
+  def handle_event("search", %{"value" => search_term}, %{assigns: %{loading: true}} = socket) do
+    {:noreply, assign(socket, search_term: search_term)}
   end
-
 
   defp init_data(socket) do
-    IO.puts "TeppelinWeb.TwitchStreamsLive.init_data: #{inspect socket.assigns}"
     assign(socket,
-           search_term: nil, 
-           streams: [],
-           loading: false,
-           timer_ref: nil,
-           streams_count: 0)
+      search_term: nil,
+      streams: [],
+      loading: false,
+      timer_ref: nil,
+      streams_count: 0
+    )
   end
-
-
 end
